56f935b0e56577fd71b6b805a9228713671779a6
ManojPrasad Singh <manojprasad_singh@epam.com>
2024-04-26 19:05:22 +0530

    url shortning api design

---
+++ DSA/src/main/java/com/learning/design/UrlShortining/URLMappingEntity.java
@@ -0,0 +1,13 @@
+package com.learning.design.UrlShortining;
+//@Entity
+public class URLMappingEntity {
+   // @Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String longURL;
+    private String shortURL;
+
+    // Getters and setters
+
+    // note use  redis or other noSql db for low latency ans scaling
+}
---
+++ DSA/src/main/java/com/learning/design/UrlShortining/URLRepository.java
@@ -0,0 +1,4 @@
+package com.learning.design.UrlShortining;
+
+//@Repository
+//public interface URLRepository extends JpaRepository
