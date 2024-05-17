2f176a63bd8048294658ea86b19577be4706cb14
ManojPrasad Singh <manojprasad_singh@epam.com>
2024-05-17 19:11:59 +0530

    min coin change problem

--- DSA/src/main/java/com/learning/dp/CoinChange.java
+++ DSA/src/main/java/com/learning/dp/CoinChange.java
@@ -1,24 +1,30 @@
 package com.learning.dp;
 
+import java.util.Arrays;
+
 public class CoinChange {
  /* Time Complexity: O(N*sum)
   Auxiliary Space: O(sum)*/
-  static int count(int coins[], int n, int sum) {
-    int table[] = new int[sum + 1];
-
-    table[0] = 1;
-
-    for (int i = 0; i < n; i++)
-      for (int j = coins[i]; j <= sum; j++)
-        table[j]= table[j]+table[j - coins[i]];
-
-    return table[sum];
+    public static int coinChange(int[] coins, int amount) {
+      int[] dp = new int[amount + 1];
+      Arrays.fill(dp, amount + 1);
+      dp[0] = 0;
+      for (int c : coins) {
+          for (int j = c; j <= amount; j++) {
+              dp[j] = Math.min(dp[j], dp[j - c] + 1);// remember
+          }
+      }
+      return dp[amount] <= amount ? dp[amount] : -1;//remember
   }
   public static void main(String args[])
   {
-    int coins[] = { 1, 2, 3 };
+   /* int coins[] = { 1, 2, 3 };
     int n = coins.length;
     int sum = 4;
-    System.out.println(count(coins, n, sum));
+    System.out.println(count(coins, n, sum));*/
+      int [] arr ={1,2,5};
+      int ans=coinChange(arr,11);
+      System.out.println(ans);
+
   }
 }
